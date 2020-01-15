6edba39715bf45b84900164c254cb524b47254c8
eder <eder@MacBook-Pro-de-eder.local>
2020-01-15 16:57:53 +0100

    stop for days

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from model.connection import *
+"""test for verify if connect to bbd is ok """
 test = connection()
 test.initialize_connection()
 test.close_connection()
+
+if __name__=='__main__':
+    pass
