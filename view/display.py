39936ed06a83fb2d78f3e7d29824012a1167f9e4
eder <eder@MacBook-Pro-de-eder.local>
2020-01-15 17:59:41 +0100

    create class Display in view folder

---
+++ view/display.py
@@ -0,0 +1,7 @@
+class Display():
+    """class for all display of the program"""
+    def __init__(self):
+        pass
+
+    def show_menu(self):
+        print("choice (c) to connect at your account or (w) for create an account")
